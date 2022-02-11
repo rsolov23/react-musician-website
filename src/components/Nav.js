bf6b7a6fc8a87e4a99b28e8e49655ccb5f810a55
Rachel Solov <rsolov23@yahoo.com>
2022-02-11 12:22:45 -0600

    added navbar component

---
+++ src/components/Nav.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+import {
+  chakra,
+  Box,
+  Flex,
+  useColorModeValue,
+  VisuallyHidden,
+  HStack,
+  Button,
+  useDisclosure,
+  VStack,
+  IconButton,
+  CloseButton,
+  Image,
+} from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import logo from "../assets/images/logo.png";
+
+function NavBar() {
+  const bg = useColorModeValue("white", "gray.800");
+  const mobileNav = useDisclosure();
+
+  return (
+    <React.Fragment>
+      <chakra.header
+        bg={bg}
+        w="full"
+        px={{ base: 2, sm: 4 }}
+        py={4}
+        shadow="md"
+      >
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <Flex>
+            <chakra.a
+              href="/"
+              title="Choc Home Page"
+              display="flex"
+              alignItems="center"
+            >
+              <Image src={logo}    boxSize='35px' />
+              <VisuallyHidden>Martell Schaub</VisuallyHidden>
+            </chakra.a>
+            <chakra.h1 fontSize="xl" fontWeight="medium" ml="2">
+              Martell Schaub
+            </chakra.h1>
+          </Flex>
+          <HStack display="flex" alignItems="center" spacing={1}>
+            <HStack
+              spacing={1}
+              mr={1}
+              color="brand.500"
+              display={{ base: "none", md: "inline-flex" }}
+            >
+              <Button variant="ghost">Home</Button>
+              <Button variant="ghost">About</Button>
+              <Button variant="ghost">Music</Button>
+              <Button variant="ghost">Blog</Button>
+              <Button variant="ghost">Contact</Button>
+            </HStack>
+
+            <Box display={{ base: "inline-flex", md: "none" }}>
+              <IconButton
+                display={{ base: "flex", md: "none" }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color={useColorModeValue("gray.800", "inherit")}
+                variant="ghost"
+                icon={<AiOutlineMenu />}
+                onClick={mobileNav.onOpen}
+              />
+
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? "flex" : "none"}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg={bg}
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
+              >
+                <CloseButton
+                  aria-label="Close menu"
+                  onClick={mobileNav.onClose}
+                />
+
+                <Button w="full" variant="ghost">
+                  Home
+                </Button>
+                <Button w="full" variant="ghost">
+                  About
+                </Button>
+                <Button w="full" variant="ghost">
+                  Music
+                </Button>
+                <Button w="full" variant="ghost">
+                  Blog
+                </Button>
+                <Button w="full" variant="ghost">
+                  Contact
+                </Button>
+              </VStack>
+            </Box>
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </React.Fragment>
+  );
+}
+export default NavBar;

28732b5d939f57be469299382badc92b8824e5ba
Rachel Solov <rsolov23@yahoo.com>
2022-06-02 17:43:11 -0500

    added chakra ui theme to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "",
+      200: "",
+      300: "",
+      400: "",
+      500: "",
+      600: "",
+      700: "",
+      800: "",
+      900: "",
+    },
+  },
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );
-
-

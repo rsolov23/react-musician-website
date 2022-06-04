8ac529b0c600d86f96684770226ee37751c7fca3
Rachel Solov <rsolov23@yahoo.com>
2022-06-04 12:30:25 -0500

    added initial blog card

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,98 @@
 import React from "react";
+import {
+  chakra,
+  Box,
+  Image,
+  Flex,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
 
-function Blog() {
-  return <div>Blog</div>;
+export default function Blog() {
+  return (
+    <Flex
+      bg={useColorModeValue("#F9FAFB", "gray.600")}
+      p={50}
+      w="full"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box
+        mx="auto"
+        rounded="lg"
+        shadow="md"
+        bg={useColorModeValue("white", "gray.800")}
+        maxW="2xl"
+      >
+        <Image
+          roundedTop="lg"
+          w="full"
+          h={64}
+          fit="cover"
+          src="https://images.pexels.com/photos/1580464/pexels-photo-1580464.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=627&w=1200"
+          alt="Article"
+        />
+
+        <Box p={6}>
+          <Box>
+            <chakra.span
+              fontSize="xs"
+              textTransform="uppercase"
+              color={useColorModeValue("brand.600", "brand.400")}
+            >
+             Blog
+            </chakra.span>
+            <Link
+              display="block"
+              color={useColorModeValue("gray.800", "white")}
+              fontWeight="bold"
+              fontSize="2xl"
+              mt={2}
+              _hover={{ color: "gray.600", textDecor: "underline" }}
+            >
+              Blog topic
+            </Link>
+            <chakra.p
+              mt={2}
+              fontSize="sm"
+              color={useColorModeValue("gray.600", "gray.400")}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+              parturient et sem ipsum volutpat vel. Natoque sem et aliquam
+              mauris egestas quam volutpat viverra. In pretium nec senectus
+              erat. Et malesuada lobortis.
+            </chakra.p>
+          </Box>
+
+          <Box mt={4}>
+            <Flex alignItems="center">
+              <Flex alignItems="center">
+                <Image
+                  h={10}
+                  fit="cover"
+                  rounded="full"
+                  src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60"
+                  alt="Avatar"
+                />
+                <Link
+                  mx={2}
+                  fontWeight="bold"
+                  color={useColorModeValue("gray.700", "gray.200")}
+                >
+                  John Doe
+                </Link>
+              </Flex>
+              <chakra.span
+                mx={1}
+                fontSize="sm"
+                color={useColorModeValue("gray.600", "gray.300")}
+              >
+                21 SEP 2021
+              </chakra.span>
+            </Flex>
+          </Box>
+        </Box>
+      </Box>
+    </Flex>
+  );
 }
-
-export default Blog;

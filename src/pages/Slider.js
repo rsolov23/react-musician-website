1514f02afb9f932965907005132f2e56d83ad873
Rachel Solov <rsolov23@yahoo.com>
2022-06-04 12:13:50 -0500

    added testimonial slider

---
+++ src/pages/Slider.js
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import {
+  Text,
+  Box,
+  Flex,
+  useColorModeValue,
+  Image,
+  HStack,
+  Stack,
+} from "@chakra-ui/react";
+import { Icon } from "@iconify/react";
+export default function Slider() {
+  const arrowStyles = {
+    cursor: "pointer",
+    pos: "absolute",
+    top: "50%",
+    w: "auto",
+    mt: "-22px",
+    p: "16px",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "18px",
+    transition: "0.6s ease",
+    borderRadius: "0 3px 3px 0",
+    userSelect: "none",
+    _hover: {
+      opacity: 0.8,
+      bg: "black",
+    },
+  };
+
+  const slides = [
+    {
+      name: "Name",
+      review:
+        "󠀢Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit 󠀢Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "Second Slide",
+      review:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "Third Slide",
+      review:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur Praesent commodo cursus magna, vel scelerisque nisl consectetur Praesent commodo cursus magna, vel scelerisque nisl consectetur Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+    {
+      name: "Fourth Slide",
+      review:
+        "Nulla vitae elit libero, a pharetra augue mollis interdum Nulla vitae elit libero, a pharetra augue mollis interdum Nulla vitae elit libero, a pharetra augue mollis interdum Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      name: "Fifth Slide",
+      review:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slidesCount = slides.length;
+
+  const prevSlide = () => {
+    setCurrentSlide((s) => (s === 0 ? slidesCount - 1 : s - 1));
+  };
+  const nextSlide = () => {
+    setCurrentSlide((s) => (s === slidesCount - 1 ? 0 : s + 1));
+  };
+  const setSlide = (slide) => {
+    setCurrentSlide(slide);
+  };
+  const carouselStyle = {
+    transition: "all .5s",
+    ml: `-${currentSlide * 100}%`,
+  };
+
+  return (
+    <Flex
+      w="full"
+      bg={useColorModeValue("gray.200", "gray.600")}
+      p={20}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="15"
+    >
+      <Flex w="full" pos="relative" overflow="hidden">
+        <Flex h="300px" w="full" {...carouselStyle}>
+          {slides.map((slide, sid) => (
+            <Box
+              key={`slide-${sid}`}
+              boxSize="full"
+              shadow="md"
+              flex="none"
+              bg={useColorModeValue("blue.200", "blue.600")}
+            >
+              <Text
+                color="white"
+                fontSize="xs"
+                p="8px 12px"
+                pos="absolute"
+                top="0"
+              >
+                {sid + 1} / {slidesCount}
+              </Text>
+              <Stack
+                p="8px 12px"
+                bottom="24px"
+                textAlign="center"
+                w="full"
+                mb="8"
+                color="white"
+                mt={45}
+              >
+                <Text fontSize="2xl">{slide.name}</Text>
+
+                <Text fontSize="lg">
+                  <Icon icon="fa:quote-left" color="black" />
+                  {slide.review}
+                  <Icon icon="fa:quote-right" color="black" right="0" />
+                </Text>
+              </Stack>
+            </Box>
+          ))}
+        </Flex>
+        <Text {...arrowStyles} left="0" onClick={prevSlide}>
+          &#10094;
+        </Text>
+        <Text {...arrowStyles} right="0" onClick={nextSlide}>
+          &#10095;
+        </Text>
+        <HStack justify="center" pos="absolute" bottom="8px" w="full">
+          {Array.from({ length: slidesCount }).map((_, slide) => (
+            <Box
+              key={`dots-${slide}`}
+              cursor="pointer"
+              boxSize={["7px", , "15px"]}
+              m="0 2px"
+              bg={currentSlide === slide ? "blackAlpha.800" : "blackAlpha.500"}
+              rounded="50%"
+              display="inline-block"
+              transition="background-color 0.6s ease"
+              _hover={{ bg: "blackAlpha.800" }}
+              onClick={() => setSlide(slide)}
+            ></Box>
+          ))}
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+}

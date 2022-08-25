794023986e76652af12bb726367b50dffb3ba920
Rachel Solov <rsolov23@yahoo.com>
2022-08-25 18:13:43 -0500

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Header from "./components/Header";
 import Navigation from "./components/Navigation";
@@ -8,20 +8,32 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Services from "./pages/Services";
 import Footer from "./components/Footer";
-
+import Preloader from "./components/Pre";
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <Router>
       {/* <Header /> */}
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/appointments" element={<Appointments />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-      <Footer />
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
